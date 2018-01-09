62ddfcf2507a662a760bf2f894dd63f3183cc762
Larry Finlay <Larry.Finlay@live.com>
2018-01-09 16:06:02 -0500

    adding csvToJson

---
+++ NodeJs/csvToJson/app.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataFolder = path.join(__dirname, 'data');
+const infile = path.join(dataFolder, 'customer-data.csv');
+const outfile = path.join(dataFolder, 'customer-data.json');
+
+// method 1
+function method1(infile, callback) {
+    const csv2json = require('csvtojson');
+    csv2json()
+        .fromFile(infile)
+        .on('end_parsed', function (jsonObjArray) {
+            callback(JSON.stringify(jsonObjArray));
+        });
+}
+
+// method 2
+function method2(infile, callback) {
+    //require the csvtojson converter class 
+    var Converter = require("csvtojson").Converter;
+    // create a new converter object
+    var converter = new Converter({});
+
+    // call the fromFile function which takes in the path to your 
+    // csv file as well as a callback function
+    converter.fromFile(infile, function (err, result) {
+        // if an error has occured then handle it
+        if (err) {
+            console.log("An Error Has Occured");
+            console.log(err);
+            callback("[]");
+        }
+        callback(JSON.stringify(result));
+        console.log("done");
+    });
+}
+
+//method1(infile, (result) => {
+//    fs.writeFileSync(outfile, result);
+//});
+
+method2(infile, (result) => {
+    fs.writeFileSync(outfile, result);
+});
